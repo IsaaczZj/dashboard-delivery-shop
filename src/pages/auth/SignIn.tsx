091039ea8b63531085750dd70b289712f7931b44
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-08-11 14:59:45 -0300

    feat:validating login form and adding toast

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,8 +1,30 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
 
+const signInForm = z.object({
+  email: z.email("Digite um email válido"),
+});
+type SignInForm = z.infer<typeof signInForm>;
 export function SignIn() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm<SignInForm>({ resolver: zodResolver(signInForm) });
+
+  async function handleLogin(data: SignInForm) {
+    try {
+      toast.success("Enviamos um link de autenticação para seu email");
+    } catch (error) {
+      // toast.error("Credenciais inválidas")
+    }
+    console.log(data);
+  }
   return (
     <div className="p-8">
       <div className="flex w-[450px] flex-col justify-center gap-6">
@@ -14,12 +36,19 @@ export function SignIn() {
             Acompanhe suas vendas e informações pelo painel
           </p>
         </header>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
           <div className="space-y-2">
             <Label htmlFor="email">Seu e-email</Label>
-            <Input id="email" type="email" />
+            <Input id="email" type="" {...register("email")} />
+            {errors.email?.message && (
+              <p className="text-center text-red-700">Email inválido</p>
+            )}
           </div>
-          <Button type="submit" className="w-full cursor-pointer">
+          <Button
+            type="submit"
+            className="h-11 w-full cursor-pointer"
+            disabled={isSubmitting}
+          >
             Acessar painel
           </Button>
         </form>
