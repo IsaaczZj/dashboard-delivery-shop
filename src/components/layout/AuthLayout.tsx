01fbfa23ce15d23fe9d0b08907961b38e515bdfe
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-08-11 14:37:50 -0300

    feat:page login

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,14 +1,17 @@
 import { Pizza } from "lucide-react";
 import { Outlet } from "react-router";
-
+import img from "@/assets/img-login.svg";
 export function AuthLayout() {
   return (
     <div className="grid min-h-screen grid-cols-2">
       <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
         <div className="text-foreground flex items-center gap-3 text-lg font-medium">
           <Pizza className="size-7" />
+
           <span className="text-xl font-semibold">Pizza Shop</span>
         </div>
+        <img src={img} alt="" />
+
         <span>
           Painel do parceiro &copy; Delivery Shop - {new Date().getFullYear()}
         </span>
--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,3 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  return (
+    <div className="p-8">
+      <div className="flex w-[450px] flex-col justify-center gap-6">
+        <header className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-muted-foreground text-lg">
+            Acompanhe suas vendas e informações pelo painel
+          </p>
+        </header>
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-email</Label>
+            <Input id="email" type="email" />
+          </div>
+          <Button type="submit" className="w-full cursor-pointer">
+            Acessar painel
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
