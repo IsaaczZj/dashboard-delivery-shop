502fc46153acc1372f7a39c4577c3df3abce54ba
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-08-17 12:00:13 -0300

    feat:adding skeleton and loading states to requests

--- src/components/AccontMenu.tsx
+++ src/components/AccontMenu.tsx
@@ -1,4 +1,4 @@
-import { Building, ChevronDown, LogOut } from "lucide-react";
+import { Building, ChevronDown, Divide, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import {
   DropdownMenu,
@@ -12,17 +12,18 @@ import { api } from "@/lib/axios";
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "@/api/get-profile";
 import { getManagerRestaurant } from "@/api/get-managed-restaurant";
+import { Skeleton } from "./ui/skeleton";
 
-interface GetProfileResponse {}
 export function AccontMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ["profile"],
     queryFn: getProfile,
   });
-  const { data: managedRestuarant } = useQuery({
-    queryKey: ["managed-resturant"],
-    queryFn: getManagerRestaurant,
-  });
+  const { data: managedRestuarant, isLoading: isLoadingManagerRestaurant } =
+    useQuery({
+      queryKey: ["managed-resturant"],
+      queryFn: getManagerRestaurant,
+    });
 
   return (
     <DropdownMenu>
@@ -31,16 +32,29 @@ export function AccontMenu() {
           variant="outline"
           className="flex items-center gap-2 select-none"
         >
-          {managedRestuarant?.name}
+          {isLoadingManagerRestaurant ? (
+            <Skeleton className="h-4 w-30" />
+          ) : (
+            managedRestuarant?.name
+          )}
           <ChevronDown className="size-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="flex w-56 flex-col gap-1.5">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-muted-foreground text-xs font-normal">
-            {profile?.email}
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-20" />
+              <Skeleton className="h-4 w-30" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-muted-foreground text-xs font-normal">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
