7677e9415705cca50495c5566b4947b12ebe1b68
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-08-21 13:54:10 -0300

    fix:page redirection with cookie

--- src/components/layout/Dashboard.tsx
+++ src/components/layout/Dashboard.tsx
@@ -1,10 +1,35 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { Header } from "../Header";
+import { useEffect } from "react";
+import { api } from "@/lib/axios";
+import { error } from "console";
+import { AxiosError } from "axios";
 
 export function DashboardLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error instanceof AxiosError) {
+          const status = error.response?.status;
+          const code = error.response?.data.code;
+
+          if (status === 401 && code === "UNAUTHORIZED") {
+            navigate("/auth", { replace: true });
+          }
+        }
+      },
+    );
+    return () => {
+      api.interceptors.response.eject(interceptorId);
+    };
+  }, [navigate]);
+
   return (
     <div className="flex min-h-screen flex-col antialiased">
-      <Header/>
+      <Header />
       <div className="flex flex-1 flex-col gap-4 p-8">
         <Outlet />
       </div>
